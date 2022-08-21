bd753470280dfc9648d77a213b20c7762346694b
Mohammad-PCSX <100066111+Mohammad-PCSX@users.noreply.github.com>
2022-08-21 13:44:51 +0430

    Add files via upload

---
+++ Marker/Client.lua
@@ -0,0 +1,13 @@
+
+
+RegisterCommand("mar",function()
+    
+end)
+
+
+CreateThread(function()
+    while true do 
+        Wait(100)
+        DrawMarker(2,3065.024, 5607.903, 209.0674,0.0,0.0,0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 255, 255, 0, 50, true,false,2,true, nil,nil,false)
+    end
+end)
---
+++ Marker/fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version "cerulean"
+game "gta5"
+
+auther "Mohammad_PCSX"
+description ""
+version "1.0.0"
+
+client_script "Client.lua"
