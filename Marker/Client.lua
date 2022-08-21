6bd3478961e0f94adba4471dc48291fb68fee841
Mohammad-PCSX <100066111+Mohammad-PCSX@users.noreply.github.com>
2022-08-21 14:24:49 +0430

    Update Client.lua

--- Marker/Client.lua
+++ Marker/Client.lua
@@ -7,7 +7,7 @@ end)
 
 CreateThread(function()
     while true do 
-        Wait(100)
+        Wait(0)
         DrawMarker(2,3065.024, 5607.903, 209.0674,0.0,0.0,0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 255, 255, 0, 50, true,false,2,true, nil,nil,false)
     end
 end)
